53facefad7df628807109a573f99c1b4ba2269b0
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2018-07-27 12:13:34 -0400

    adding README and grapher

---
+++ README.md
@@ -0,0 +1,74 @@
+Dog
+------
+
+
+This is a simple programming lanuage for experimenting with causal diagram. With Dog, one can define a causal diagram using formulas and then both i) represent the diagram visually, ii) evaulate the effect of an exposure on an outcome variable. 
+
+Why? I wanted to quickly test ideas like:
+
+1) What happens to my regression coefficients when I introduce a mediator?
+2) What happens to my regression coefficients when I control for an independent factor?
+3) What's the impact of controlling inappropriately for a collider?
+etc.
+
+
+Syntax and Grammar
+----------------------
+
+`<variables>`: these represent nodes in the causal diagram. There are special variables, denoted `E` and `O` (exposure and outcome). 
+`~`: represent a causal relationship. The LHS is a child variable of all the variables on the RHS.
+`+`: assumes a linear relationship between variables
+`<floats>`: can be added or multiplied to variables
+`*`: multiply a float by a variable.
+`;`: end of line statement
+`//`: inline comment
+
+Dog programs are represented as lines of forumlas, each line describing partent-child relationships. 
+
+Example:
+
+```
+R ~ X1 + X2 + X3;
+Z ~ X4;
+Z ~ X2;
+Z ~ X4 + X5; // X4 will only be added once
+E ~ Z + R;
+Y ~ 5.0*E + Z;
+```
+
+This produces the following DAG:
+
+![dag](https://i.imgur.com/wfDongg.png)
+
+You can quickly iterate on this DAG by adding more lines, relationships or variables:
+
+```
+R ~ X1 + X2 + X3;
+Z ~ X4;
+Z ~ X2;
+Z ~ X4 + X5; // X4 will only be added once
+E ~ Z + R;
+Y ~ 5.0*E + Z + 0.5*R + X6;
+```
+
+![dag2](https://i.imgur.com/Hoy9zGb.png)
+
+
+Usage
+----------
+
+### Graphing and Plotting
+Create a Dog program and save it with a `.dg` extension, can run the following to graph it:
+
+`python3 -m dog.grapher <filename>`
+
+### Evaluation
+`python3 -m dog.evaluate <filename>`
+
+
+Future
+--------
+
+Implement the backdoor criteria to control for the correct things. 
+Introduce non-linear relationships
+Introduce different noise
---
+++ dog/grapher.py
@@ -0,0 +1,89 @@
+# evaluate.py
+
+"""
+
+1. put all undefined variables into name space
+2. traverse the AST, compute the dataset.
+
+ ex:
+   def visitBinOp(self, node):
+      left = self.visit(node.left)
+      right = self.visit(node.right)
+      return left + right
+
+    def visit_SimpleLocation(slef, node):
+        return eval('%s' % node.name)
+
+"""
+from operator import add
+from functools import reduce
+from .errors import error
+from .checker import CheckProgramVisitor
+from .ast import *
+from collections import defaultdict
+from numpy import random as random
+import numpy as np
+import pandas as pd
+from statsmodels import api as sm
+
+
+N = 1000
+noise = lambda : random.randn(N)
+
+UNDEFINED = 'undefined'
+DEFINED = 'defined'
+
+
+class Grapher():
+
+    def __init__(self, checker):
+        self.graph = checker.graph
+
+    def plot(self):
+
+        import networkx as nx
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import pylab
+        from networkx.drawing.nx_agraph import write_dot, graphviz_layout
+
+        edge_labels= {(u,v,): d['weight']
+                     for u,v,d in self.graph.edges(data=True)
+                     if d['weight'] is not None
+                     }
+        node_labels = {node:node for node in self.graph.nodes()};
+
+        pos = graphviz_layout(self.graph, prog='dot')
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=edge_labels)
+        nx.draw_networkx_labels(self.graph, pos, labels=node_labels)
+        nx.draw_networkx(self.graph, pos, node_color="#d3d3d3", node_size=1200, edge_color='black', linewidths=1.0)
+        pylab.show()
+
+
+def graph_dag(program):
+
+    from .parser import parse
+
+    ast = parse(program)
+    checker = CheckProgramVisitor()
+    checker.visit(ast)
+
+    graph = Grapher(checker)
+
+    return graph.plot()
+
+
+def main():
+    '''
+    Main program. Used for testing
+    '''
+    import sys
+
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: python3 -m dog.grapher filename\n')
+        raise SystemExit(1)
+
+    graph_dag(open(sys.argv[1]).read())
+
+if __name__ == '__main__':
+    main()
